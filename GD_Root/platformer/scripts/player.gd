ea0244237fc1a534054f559ce19c3ecc3b42f504
Annis_William <anniw544@hsd.k12.or.us>
2025-05-20 12:36:23 -0700

    fixed most dash bugs+learn wait cmd

--- GD_Root/platformer/scripts/player.gd
+++ GD_Root/platformer/scripts/player.gd
@@ -10,23 +10,24 @@ extends CharacterBody2D
 var air_dash = 0
 var phy_disable = 0
 var freeze_frame = 0
+var grav_disable = 0
 
 
 
 func _physics_process(delta):
 	#Vertical gravity
-	if !is_on_floor():
-		velocity.y += gravity
-		if velocity.y > v_cap:
-			velocity.y = v_cap
+	if grav_disable == 0:	
+		if !is_on_floor():
+			velocity.y += gravity
+			if velocity.y > v_cap:
+				velocity.y = v_cap
 				
 				
 	#Horizontal drag
-	if phy_disable == 0:
-		if velocity.x > 0:
-			velocity.x -= drag
-		elif velocity.x <0:
-			velocity.x += drag
+	if velocity.x > 0:
+		velocity.x -= drag
+	elif velocity.x <0:
+		velocity.x += drag
 	
 	
 	#Floor reset
@@ -46,16 +47,15 @@ func _physics_process(delta):
 		if air_dash > 0:
 			var Dhorizontal_direction = Input.get_axis("move_left", "move_right")
 			var Dvertical_direction = Input.get_axis("move_up", "move_down")
-			velocity.x = Dhorizontal_direction * dash_str
-			velocity.y = Dvertical_direction * dash_str
 			air_dash -= 1
 			phy_disable = 1
+			grav_disable = 1
+			velocity.x = Dhorizontal_direction * dash_str
+			velocity.y = Dvertical_direction * dash_str
+			await get_tree().create_timer(.1).timeout
+			grav_disable = 0
 			
 			
-	if Input.is_action_just_pressed("action_cancel"):
-		phy_disable = 0
-		#reset vert speed
-		velocity.y = 0
 	
 	
 	#Horizontal movement
